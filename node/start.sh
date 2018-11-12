242c519f5861fea0abc8c25055da9a8d220d1f7d
Sergey <szhdanoff@gmail.com>
2018-11-12 16:10:00 +0300

    fix rpcaddr

--- node/start.sh
+++ node/start.sh
@@ -12,10 +12,12 @@ NODE_NAME=docker.${HOSTNAME}
 
 geth \
 --datadir=/root/node/joys \
---rpcvhosts "*" \
---rpccorsdomain="*" \
 --networkid=35855456 \
 --rpc \
+--rpcvhosts "*" \
+--rpccorsdomain="*" \
+--rpcaddr "0.0.0.0" \
+--rpcapi "admin,db,personal,eth,net,web3,shh,txpool" \
 --bootnodes="enode://1750c090cd33c713c5bdba7d77e01223126fe5bde3a9829ef177a33bb911da3d1c5369ea089e1862c4c149761249c702c00fce70cefe764d6dbfc2853bf74848@95.213.151.253:30303" \
 --nat "any" \
 --ethstats=${NODE_NAME}:8382716d0b07b0a3@eth.joys.digital:3000
